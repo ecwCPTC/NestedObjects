3206240d8fc2f4e87de9191ea7213bfc06393a9a
Evan Ward <enjoievan@gmail.com>
2022-07-21 11:16:43 -0700

    Added missing property for date of birth.

--- NestedObjects/Student.cs
+++ NestedObjects/Student.cs
@@ -11,6 +11,7 @@ namespace NestedObjects
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string SchoolEmail { get; set; }
+        public DateOnly DateOfBirth { get; set; }
         public string PhoneNumber { get; set; }
 
 
